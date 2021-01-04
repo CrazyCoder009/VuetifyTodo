25463f73aab1c553186dd9fe1e11258ab990dc74
CrazyCoder009 <nairrohit3@gmail.com>
2021-01-04 13:31:11 +0530

    Converted as PWA

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="about pa-6">
-    <h1>About Vue</h1>
-    <p>CNJNKJNDSnjdkn jndksjd jndkj  jaknjas jsnk jknca kcjnskc jaksjc s CNJNKJNDSnjdkn jndksjd
-       jndkj  jaknjas jsnk jknca kcjnskc jaksjc s</p>
-       <p>CNJNKJNDSnjdkn jndksjd jndkj  jaknjas jsnk jknca kcjnskc jaksjc s CNJNKJNDSnjdkn jndksjd
-       jndkj  jaknjas jsnk jknca kcjnskc jaksjc s</p>
-       <p>CNJNKJNDSnjdkn jndksjd jndkj  jaknjas jsnk jknca kcjnskc jaksjc s CNJNKJNDSnjdkn jndksjd
-       jndkj  jaknjas jsnk jknca kcjnskc jaksjc s</p>
-       <p>CNJNKJNDSnjdkn jndksjd jndkj  jaknjas jsnk jknca kcjnskc jaksjc s CNJNKJNDSnjdkn jndksjd
-       jndkj  jaknjas jsnk jknca kcjnskc jaksjc s</p>
+    <h1>About Vuetify Todo</h1>
+    <p>This is a sample to-do note taking PWA app on the vue framework using vuetify.</p>
+    <br>
+    <p> Developed by Rohit R Nair</p>
   </div>
 </template>
