874f2d94de3e90a4dccc6728572bc5aed0ca0ea7
CrazyCoder009 <nairrohit3@gmail.com>
2021-01-04 12:57:23 +0530

    Created as requested

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vue</h1>
+    <p>CNJNKJNDSnjdkn jndksjd jndkj  jaknjas jsnk jknca kcjnskc jaksjc s CNJNKJNDSnjdkn jndksjd
+       jndkj  jaknjas jsnk jknca kcjnskc jaksjc s</p>
+       <p>CNJNKJNDSnjdkn jndksjd jndkj  jaknjas jsnk jknca kcjnskc jaksjc s CNJNKJNDSnjdkn jndksjd
+       jndkj  jaknjas jsnk jknca kcjnskc jaksjc s</p>
+       <p>CNJNKJNDSnjdkn jndksjd jndkj  jaknjas jsnk jknca kcjnskc jaksjc s CNJNKJNDSnjdkn jndksjd
+       jndkj  jaknjas jsnk jknca kcjnskc jaksjc s</p>
+       <p>CNJNKJNDSnjdkn jndksjd jndkj  jaknjas jsnk jknca kcjnskc jaksjc s CNJNKJNDSnjdkn jndksjd
+       jndkj  jaknjas jsnk jknca kcjnskc jaksjc s</p>
   </div>
 </template>
